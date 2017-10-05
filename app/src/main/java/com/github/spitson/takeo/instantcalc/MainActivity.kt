f2b68652369f1de13172cc5c11c18dd02074c6d7
Takeo Asai <spitson+github@gmail.com>
2017-10-06 07:46:27 +0900

    Add androidbootstrap

--- app/src/main/java/com/github/spitson/takeo/instantcalc/MainActivity.kt
+++ app/src/main/java/com/github/spitson/takeo/instantcalc/MainActivity.kt
@@ -2,11 +2,13 @@ package com.github.spitson.takeo.instantcalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.beardedhen.androidbootstrap.TypefaceProvider
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        TypefaceProvider.registerDefaultIconSets();
         setContentView(R.layout.activity_main)
     }
 }
